1b4105c5c6882d819565bae19ba7694dd966c6c2
Mustafa-alsalhi <alsalhimustafa340@gmail.com>
2026-04-15 03:59:55 +0300

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -23,6 +23,7 @@ return [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://jgusgy.netlify.app',
+    'https://trtree.netlify.app/',
     ],
 
     'allowed_origins_patterns' => [],
