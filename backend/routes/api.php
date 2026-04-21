ac691a1d6e8ee67db4e54cdf2697c84416426534
Mustafa-alsalhi <alsalhimustafa340@gmail.com>
2026-04-22 00:54:15 +0300

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,27 +1,27 @@
-    <?php
+<?php
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
-    use App\Http\Controllers\Api\ProductController;
-    use App\Http\Controllers\Api\CategoryController;
-    use App\Http\Controllers\Api\AuthController;
-    use App\Http\Controllers\Api\CartController;
-    use App\Http\Controllers\Api\WishlistController;
-    use App\Http\Controllers\Api\OrderController;
-    use App\Http\Controllers\Api\BannerController;
-    use App\Http\Controllers\AdminController;
-    use App\Http\Controllers\CouponController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\WishlistController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\BannerController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CouponController;
 
-    /*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | is assigned the "api" middleware group. Enjoy building your API!
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
     // Test endpoint for order creation
     Route::get('/test-order', function () {
