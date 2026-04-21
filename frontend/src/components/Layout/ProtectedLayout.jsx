d0f7e9de2c6655767d8e2c74e56471976ab71d09
Mustafa-alsalhi <alsalhimustafa340@gmail.com>
2026-04-21 23:14:04 +0300

    Update ProtectedLayout.jsx

--- frontend/src/components/Layout/ProtectedLayout.jsx
+++ frontend/src/components/Layout/ProtectedLayout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import ProtectedRoute from '../Auth/ProtectedRoute'
 import Layout from './Layout'
+import { getCurrentUser } from '../../store/slices/authSlice'
 
 const ProtectedLayout = () => {
+  const dispatch = useDispatch()
+  const token = useSelector((state) => state.auth.token)
+  const user = useSelector((state) => state.auth.user)
+
+  useEffect(() => {
+    // Fetch user data if token exists but user data is missing
+    if (token && !user) {
+      console.log('🔄 Auto-fetching user data in ProtectedLayout...')
+      dispatch(getCurrentUser())
+    }
+  }, [dispatch, token, user])
+
   return (
     <ProtectedRoute>
       <Layout>
